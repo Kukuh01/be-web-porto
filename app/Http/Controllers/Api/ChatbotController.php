e65f004761b91d9e91d028b230d9787f5ef05c38
silvanus <silvanusprasetyo@gmail.com>
2026-04-20 14:30:01 +0700

    Adding chatbot controller

---
+++ app/Http/Controllers/Api/ChatbotController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers\Api;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Http\Controllers\Controller;
+
+class ChatbotController extends Controller
+{
+    public function chat(Request $request)
+    {
+        $response = Http::post('https://n8n.silvanus.my.id/webhook-test/6d7146a2-b735-4aef-b1e4-0e751a19d061', [
+            'message' => $request->message,
+            'sessionid' => $request->sessionId,
+        ]);
+
+        return response()->json($response->json());
+    }
+}
